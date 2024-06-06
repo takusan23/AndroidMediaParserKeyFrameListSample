c931b35f50e91f4c1d064fb14fd345b9389f3657
takusan23 <takumu1200@gmail.com>
2024-06-07 03:20:29 +0900

    Initial commit

---
+++ app/src/main/java/io/github/takusan23/androidmediaparserkeyframelistsample/MainActivity.kt
@@ -0,0 +1,85 @@
+package io.github.takusan23.androidmediaparserkeyframelistsample
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import io.github.takusan23.androidmediaparserkeyframelistsample.ui.theme.AndroidMediaParserKeyFrameListSampleTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidMediaParserKeyFrameListSampleTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val seekPositionList = remember { mutableStateOf(listOf<Long>()) }
+
+    fun analyze(uri: Uri?) {
+        uri ?: return
+        scope.launch {
+            // InputStream を開く
+            seekPositionList.value = MediaContainerAnalyzer.analyze(onCreateInputStream = { context.contentResolver.openInputStream(uri)!! })
+        }
+    }
+
+    val filePicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri -> analyze(uri) }
+    )
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) }) }
+    ) { innerPadding ->
+
+        Column(Modifier.padding(innerPadding)) {
+
+            Button(onClick = { filePicker.launch(PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.VideoOnly)) }) {
+                Text(text = "動画の選択")
+            }
+
+            Text(text = "キーフレーム一覧")
+
+            LazyColumn {
+                items(seekPositionList.value) {
+                    Text(text = "keyFrame = $it us")
+                    HorizontalDivider()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/io/github/takusan23/androidmediaparserkeyframelistsample/MediaContainerAnalyzer.kt
@@ -0,0 +1,103 @@
+package io.github.takusan23.androidmediaparserkeyframelistsample
+
+import android.media.MediaCodec
+import android.media.MediaParser
+import android.media.MediaParser.SeekableInputReader
+import androidx.core.util.component2
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+
+/** MediaParser を使ってコンテナフォーマットを解析する */
+object MediaContainerAnalyzer {
+
+    suspend fun analyze(onCreateInputStream: () -> InputStream): List<Long> = withContext(Dispatchers.IO) {
+
+        // MediaParser を作る
+        var seekMap: MediaParser.SeekMap? = null
+        // onSeekFound が来たら解析終わってほしいので
+        var isFoundSeekMap = false
+        // パース結果コールバック
+        val output = object : MediaParser.OutputConsumer {
+
+            // 中身には興味がないので適当に入れ物だけ用意
+            private val tempByteArray = ByteArray(4096)
+
+            override fun onSeekMapFound(p0: MediaParser.SeekMap) {
+                // 解析してシークできる位置が分かった
+                seekMap = p0
+                isFoundSeekMap = true
+            }
+
+            override fun onTrackCountFound(p0: Int) {
+                // do nothing
+            }
+
+            override fun onTrackDataFound(p0: Int, p1: MediaParser.TrackData) {
+                // do nothing
+            }
+
+            override fun onSampleDataFound(p0: Int, p1: MediaParser.InputReader) {
+                // SeekMap が欲しいだけなのだが、InputReader#read しないと MediaParser#advance で止まってしまうので
+                // サンプル通りに InputReader#read している。
+                // SeekMap が欲しいだけで中身には興味がないので tempByteArray に上書きしている
+                val readSize = p1.length.toInt()
+                p1.read(tempByteArray, 0, minOf(tempByteArray.size, readSize))
+            }
+
+            override fun onSampleCompleted(p0: Int, p1: Long, p2: Int, p3: Int, p4: Int, p5: MediaCodec.CryptoInfo?) {
+                // do nothing
+            }
+        }
+        // InputStream を MediaParser.InputReader で使う
+        val input = InputStreamSeekableInputReader(onCreateInputStream)
+
+        // MP4 と WebM のコンテナを解析する
+        val mediaParser = MediaParser.create(output, MediaParser.PARSER_NAME_MP4, MediaParser.PARSER_NAME_MATROSKA)
+        while (!isFoundSeekMap && mediaParser.advance(input)) {
+            // SeekMap が取れるまで while 回す
+        }
+
+        mediaParser.release()
+        input.close()
+
+        // 流石にないはず
+        seekMap ?: return@withContext emptyList()
+
+        // SeekMap 取れたら、1 秒ごとにシークできる位置はどこかを問いただす
+        return@withContext (0 until seekMap!!.durationMicros step 1_000_000) // マイクロ秒注意
+            .map { timeUs -> seekMap!!.getSeekPoints(timeUs).component2() } // 次のシーク位置が欲しい
+            .map { it.timeMicros }
+            .distinct() // 同じ値（キーフレーム間隔が 1 秒以上なら同じ値が入ってくることある）
+    }
+
+    /** MediaParser.Input の InputStream 実装例 */
+    class InputStreamSeekableInputReader(private val onCreateInputStream: () -> InputStream) : SeekableInputReader {
+
+        /** InputStream。[seekToPosition]が呼び出された際には作り直す */
+        private var currentInputStream = onCreateInputStream()
+
+        /** read する前に available を呼ぶことでファイルの合計サイズを出す */
+        private val fileSize = currentInputStream.available().toLong()
+
+        override fun read(p0: ByteArray, p1: Int, p2: Int): Int = currentInputStream.read(p0, p1, p2)
+
+        override fun getPosition(): Long = fileSize - currentInputStream.available()
+
+        override fun getLength(): Long = fileSize
+
+        override fun seekToPosition(p0: Long) {
+            println("seekToPosition")
+            // ContentResolver#openInputStream だと mark/reset が使えない
+            // InputStream を作り直す
+            currentInputStream.close()
+            currentInputStream = onCreateInputStream()
+            currentInputStream.skip(p0)
+        }
+
+        /** InputStream を閉じる */
+        fun close() {
+            currentInputStream.close()
+        }
+    }
+}
